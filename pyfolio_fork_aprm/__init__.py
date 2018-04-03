5db835c03571679055cc98d17037c1b431afa4df
kevinheavey <heaveykevin2@gmail.com>
2018-04-03 21:42:56 +0100

    remove reference to _version.py

--- pyfolio_fork_aprm/__init__.py
+++ pyfolio_fork_aprm/__init__.py
@@ -12,7 +12,6 @@ from . import perf_attrib
 
 from .tears import *  # noqa
 from .plotting import *  # noqa
-from ._version import get_versions
 
 try:
     from . import bayesian
@@ -21,9 +20,6 @@ except ImportError:
         "Could not import bayesian submodule due to missing pymc3 dependency.",
         ImportWarning)
 
-
-__version__ = get_versions()['version']
-del get_versions
 
 __all__ = ['utils', 'timeseries', 'pos', 'txn', 'bayesian',
            'interesting_periods', 'capacity', 'round_trips',
